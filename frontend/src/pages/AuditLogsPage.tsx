d4ffe0b98b60ef1366f090859d47a6a6c68cdf13
Adith Rajeev <adith.random.x@gmail.com>
2026-06-25 11:29:07 +0530

    add initial pages

---
+++ frontend/src/pages/AuditLogsPage.tsx
@@ -0,0 +1,9 @@
+function AuditLogsPage() {
+  return (
+    <div>
+      <h1>Audit Logs Page</h1>
+    </div>
+  );
+}
+
+export default AuditLogsPage;
---
+++ frontend/src/pages/IncidentsPage.tsx
@@ -0,0 +1,9 @@
+function IncidentsPage() {
+  return (
+    <div>
+      <h1>Incidents Page</h1>
+    </div>
+  );
+}
+
+export default IncidentsPage;
