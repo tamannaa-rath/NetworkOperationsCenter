2835f0d6488f65ffb5e00ad6acc236d09dbbaf56
Tamanna Rath <tamannarath01@gmail.com>
2026-07-01 22:58:20 +0530

    feat: add environment config for backend

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 const app = require("./app");
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
 });
